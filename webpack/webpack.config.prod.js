8d34dad6816ea0a5ad3c7ba9612209a747334d43
e6voe9 <george.bakhanovych@gmail.com>
2022-01-27 10:49:19 +0100

    indent using tabs

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,40 +4,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
-  stats: 'all',
-  bail: true,
-  output: {
-    filename: 'js/[name].js',
-    chunkFilename: 'js/[name].js',
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/index.css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.s?css/i,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 2,
-            import: true,
-            url: false
-          }
-        }, 'postcss-loader', 'sass-loader'],
-      },
+	mode: 'production',
+	stats: 'all',
+	bail: true,
+	output: {
+		filename: 'js/[name].js',
+		chunkFilename: 'js/[name].js',
+	},
+	plugins: [
+		new Webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/index.css',
+		}),
+	],
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.s?css/i,
+				use: [MiniCssExtractPlugin.loader, {
+					loader: 'css-loader',
+					options: {
+						importLoaders: 2,
+						import: true,
+						url: false
+					}
+				}, 'postcss-loader', 'sass-loader'],
+			},
 
-    ],
-  },
+		],
+	},
 });
