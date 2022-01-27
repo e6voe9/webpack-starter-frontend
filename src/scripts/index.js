25ca385c6053e367d867a686d4f16ae951273818
e6voe9 <george.bakhanovych@gmail.com>
2022-01-27 10:56:07 +0100

    lazy load added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,5 +4,29 @@ if (process.env.NODE_ENV === 'development') {
 	require('../index.html');
 }
 
+// --------- LAZY LOAD IMAGES ---------
+// const images = document.querySelectorAll('img');
+// const options = {
+// 	root: null,
+// 	rootMargin: '0px',
+// 	threshold: 0.1
+// }
+
+// function handleImg(myImg, observer) {
+// 	myImg.forEach(myImgSingle => {
+// 		if (myImgSingle.intersectionRatio > 0) {
+// 			loadImage(myImgSingle.target);
+// 		}
+// 	})
+// }
+
+// function loadImage(image) {
+// 	image.src = image.getAttribute('data-src');
+// 	observer.unobserve(image);
+// }
+// const observer = new IntersectionObserver(handleImg, options);
+// images.forEach(img => {
+// 	observer.observe(img);
+// })
 
 console.log('loaded successfully');
